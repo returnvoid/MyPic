089e3083c6d04208a77967f2e90190c178f0451c
Gonzalo Geraldo <geraldo.gonzalo@gmail.com>
2013-07-04 11:01:26 -0400

    First commit

--- MyPic/src/main/java/cl/returnvoid/mypic/MainActivity.java
+++ MyPic/src/main/java/cl/returnvoid/mypic/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends Activity{
     private RelativeLayout.LayoutParams params;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //this is main activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
