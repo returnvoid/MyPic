f514449a2feb41e35b27ae1bf3ea65573d2e66be
Gonzalo Geraldo <geraldo.gonzalo@gmail.com>
2013-07-22 03:48:33 -0400

    mainactivity y surfaceview

--- MyPic/src/main/java/cl/returnvoid/mypic/MainActivity.java
+++ MyPic/src/main/java/cl/returnvoid/mypic/MainActivity.java
@@ -1,174 +1,29 @@
 package cl.returnvoid.mypic;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.hardware.Camera;
-import android.media.MediaPlayer;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Activity;
-import android.os.Environment;
+import android.content.Intent;
+import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.AttributeSet;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
-class PreviewCamera extends SurfaceView implements SurfaceHolder.Callback{
-    public static final String PREVIEW_CAMERA = "PREVIEW_CAMERA";
-    private Camera camera;
-    private Boolean cameraConfigured = false;
-    public PreviewCamera(Context context, AttributeSet attrs){
-        super(context, attrs);
-        //setWillNotDraw(false);
-        getHolder().addCallback(this);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas){
-        Log.w(this.getClass().getName(), "On Draw Called");
-    }
-
-    public void previewAsBitmap(){
-        if(camera!=null&&getHolder().getSurface()!=null){
-            camera.takePicture(shutterCallback, pictureCallback, jpegCallBack);
-        }
-
-    }
-
-    Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
-        @Override
-        public void onShutter() {
-            MediaPlayer _shootMP = MediaPlayer.create(getContext(), Uri.parse("file:///system/media/audio/ui/camera_click.ogg"));
-            _shootMP.start();
-        }
-    };
-
-    Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
-        @Override
-        public void onPictureTaken(byte[] bytes, Camera camera) {
-
-        }
-    };
-
-    Camera.PictureCallback jpegCallBack = new Camera.PictureCallback() {
-        @Override
-        public void onPictureTaken(byte[] bytes, Camera camera) {
-            File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyPic");
-            if(!folder.exists()){
-                folder.mkdir();
-            }
-            int number = folder.listFiles().length;;
-
-            String photoName = "mypic" + number + ".jpg";
-            File photo = new File(folder, photoName);
-
-            Uri imageSaved = Uri.fromFile(photo);
-            Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageSaved);
-            try {
-                FileOutputStream fos = new FileOutputStream(photo.getPath());
-                fos.write(bytes);
-                fos.close();
-            }
-            catch (java.io.IOException e) {
-                Log.e("PictureDemo", "Exception in photoCallback", e);
-            }
-            MediaScannerConnection.scanFile(getContext(),
-                    new String[]{photo.getPath()}, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        public void onScanCompleted(String path, Uri uri) {
-                            Log.d(PREVIEW_CAMERA, "Scanned " + path + ":");
-                            Log.d(PREVIEW_CAMERA, "-> uri=" + uri);
-                        }
-                    }
-            );
-            camera.startPreview();
-        }
-    };
+import cl.returnvoid.mypic.fragments.PreviewCameraFragment;
 
 
-    private void initPreview(int width, int height) {
-        if (camera!=null && getHolder().getSurface()!=null) {
-            try {
-                camera.setPreviewDisplay(getHolder());
-            }
-            catch (Throwable t) {
-                Log.d("T", t.toString());
-            }
-
-            if (!cameraConfigured) {
-                Camera.Parameters parameters=camera.getParameters();
-                Camera.Size size=getBestPreviewSize(width, height, parameters);
-
-                if (size!=null) {
-                    parameters.setPreviewSize(size.width, size.height);
-                    camera.setParameters(parameters);
-                    cameraConfigured=true;
-                }
-            }
-        }
-    }
-
-    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
-        Camera.Size result=null;
-        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-            if (size.width<=width && size.height<=height) {
-                if (result==null) {
-                    result=size;
-                }
-                else {
-                    int resultArea=result.width*result.height;
-                    int newArea=size.width*size.height;
-
-                    if (newArea>resultArea) {
-                        result=size;
-                    }
-                }
-            }
-        }
-
-        return(result);
-    }
-
-    private void startPreview() {
-        if (cameraConfigured && camera != null) {
-            camera.startPreview();
-        }
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
-        initPreview(width, height);
-        Log.d("Callback", "startPreview auto");
-        startPreview();
-    }
-
-    public void surfaceCreated(SurfaceHolder holder){
-        camera = Camera.open();
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder){
-        camera.stopPreview();
-        camera = null;
-    }
-}
-public class MainActivity extends Activity{
+public class MainActivity extends FragmentActivity {
     private String ACTIVITY_TAG = "ACTIVITY_TAG";
     private Boolean shuterStatus = false;
-    private PreviewCamera preview;
+    public PreviewCameraFragment previewCameraFragment;
     private Animation translate;
+    public Intent imageIntent;
     private RelativeLayout.LayoutParams params;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -176,15 +31,14 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        preview = (PreviewCamera) findViewById(R.id.preview);
-        preview.setVisibility(View.GONE);
+        previewCameraFragment = new PreviewCameraFragment();
 
         Log.d(ACTIVITY_TAG, "onCreate");
 
         Button shutter = (Button) findViewById(R.id.shutter);
         shutter.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(final View view) {
+            public void onClick(View view) {
                 final Button btn = (Button) view;
                 try {
                     translate = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animate_shutter_button);
@@ -198,9 +52,12 @@ public class MainActivity extends Activity{
                             @Override
                             public void onAnimationEnd(Animation animation) {
                                 shuterStatus = true;
-                                params.topMargin = 200 + params.topMargin;
-                                btn.setLayoutParams(params);
-                                preview.setVisibility(View.VISIBLE);
+                                //params.topMargin = 200 + params.topMargin;
+                                //btn.setLayoutParams(params);
+                                btn.setVisibility(View.GONE);
+
+                                getSupportFragmentManager().beginTransaction().add(R.id.preview_camera_container, previewCameraFragment).commit();
+                                //preview.setVisibility(View.VISIBLE);
                                 Log.d(ACTIVITY_TAG, "animationEnded");
                             }
 
@@ -217,9 +74,11 @@ public class MainActivity extends Activity{
                     Log.d(ACTIVITY_TAG, "startAnimation");
                     view.startAnimation(translate);
                 }else{
-                    //capture image from surface view
-                    preview.previewAsBitmap();
-                    Log.d(ACTIVITY_TAG, "picture saved");
+                    /*imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    previewCameraFragment.getPreview().previewAsBitmap(imageIntent);*/
+                    //f.getPreview().previewAsBitmap(imageIntent);
+
+                    //Log.d(ACTIVITY_TAG, "picture saved:" + previewCameraFragment.getPreview().imageSaved);
                 }
             }
         });
